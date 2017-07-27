5befadeabb62845d1b91341ddc9e6ee7977c6bb0
xugc <guchengxu1988@163.com>
2017-07-27 17:30:12 +0800

    断路器配置

--- service-consumer/src/main/java/com/gc/cloud/service/consumer/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/com/gc/cloud/service/consumer/ServiceConsumerApplication.java
@@ -2,6 +2,7 @@ package com.gc.cloud.service.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ServiceConsumerApplication {
 	@Bean
 	@LoadBalanced
---
+++ service-consumer/src/main/java/com/gc/cloud/service/consumer/service/UserService.java
@@ -0,0 +1,21 @@
+/**
+ * @Title UserService.java
+ * @Package com.gc.cloud.service.consumer.service
+ * @Description TODO
+ * Copyright: Copyright (c) 2017 
+ * Company:*******
+ * 
+ * @author 徐故成
+ * @date 2017-7-28 上午10:39:02
+ * @version V1.0
+ */
+package com.gc.cloud.service.consumer.service;
+
+/**
+ * @ClassName UserService
+ * @Description TODO
+ * 
+ */
+public interface UserService {
+	public String getUser(String name);
+}
